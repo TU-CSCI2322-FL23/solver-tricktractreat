fcc4d95a221053b772c55d485406266fca79ef6b
Gayatri Rajamony <gayatrirajamony@gmail.com>
2023-11-01 12:47:43 -0500

    fixed gameState

--- Ultimate.hs
+++ Ultimate.hs
@@ -1,9 +1,10 @@
 data Player = X | O
 data Winner = Champ Player | Tie 
 data SubBoard = InProgress [[Maybe Player]] | Finished Winner
-gameState :: (Player, Maybe SubBoard, [SubBoard])
+type gameState = (Player, Maybe SubBoard, [SubBoard])
+-- gameState should be a type, not a variable type def
 
-functions for
--- is it complete
--- making a move
--- determining what the next board is 
+-- functions for
+  -- is it complete
+  -- making a move
+  -- determining what the next board is 
