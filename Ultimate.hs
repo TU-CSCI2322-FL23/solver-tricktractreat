c28c0f12923075f95349c4b92f8cd9078a683305
Gayatri Rajamony <gayatrirajamony@gmail.com>
2023-11-01 13:02:38 -0500

    Finalized types

--- Ultimate.hs
+++ Ultimate.hs
@@ -1,8 +1,10 @@
-data Player = X | O
-data Winner = Champ Player | Tie 
-data SubBoard = InProgress [[Maybe Player]] | Finished Winner
-type gameState = (Player, Maybe SubBoard, [SubBoard])
--- gameState should be a type, not a variable type def
+data Player = X | O deriving (Show, Eq)
+data Winner = Champ Player | Tie deriving (Show, Eq)
+data SubBoard = InProgress [[Maybe Player]] | Finished Winner deriving (Show, Eq)
+type Coord = (Int, Int)
+type BigMove = (Coord, Coord) -- first coord is the location of the SUBBOARD second is for the spot on the subboard
+type GameState = (Player, Maybe Coord, [[SubBoard]])
+-- GameState should be a type, not a variable type def
 
 -- functions for
   -- is it complete
