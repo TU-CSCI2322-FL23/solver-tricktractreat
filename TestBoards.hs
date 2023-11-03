71471fd4db5a366bbe3b3f84bd5a1cc08f56a4e2
Blake Deery <bdeery@trinity.edu>
2023-11-03 15:15:33 -0500

    TestBoards

---
+++ TestBoards.hs
@@ -0,0 +1,34 @@
+import Ultimate
+
+emptySubBoard, winnerXSubBoard, winnerOSubBoard :: SubBoard
+emptySubBoard   = replicate 3 (replicate 3 Nothing)
+allXs = replicate 3 (replicate 3 (Just X))
+allOs = replicate 3 (replicate 3 (Just O))
+tiedSubBoard = replicate 3 (replicate 3 "-")
+
+{-
+[X] [ ] [O] | [O] [ ] [ ] | [X] [O] [ ]
+[O] [X] [X] | [X] [X] [ ] | [ ] [ ] [ ]
+[ ] [ ] [O] | [ ] [ ] [ ] | [O] [O] [ ]
+------------|-------------|------------
+[ ] [X] [X] | [ ] [O] [ ] | [O] [ ] [ ]
+[ ] [O] [ ] | [ ] [ ] [ ] | [ ] [O] [ ]
+[ ] [ ] [ ] | [X] [X] [X] | [ ] [ ] [O]
+------------|-------------|------------
+[ ] [ ] [X] | [ ] [ ] [ ] | [X] [ ] [ ]
+[ ] [ ] [X] | [ ] [ ] [ ] | [ ] [ ] [ ]
+[ ] [ ] [X] | [O] [ ] [X] | [O] [O] [O]
+-}
+
+testSubBoard1_1, testSubBoard2_1, testSubBoard3_1, testSubBoard1_2, testSubBoard2_3 :: SubBoard
+testSubBoard1_1 = InProgress [[Just X, Nothing, Just O], [Just O, Just X, Just X], [Nothing, Nothing, Just O]]
+testSubBoard2_1 = InProgress [[Just O, Nothing, Nothing], [Just X, Just X, Nothing], [Nothing, Nothing, Nothing]]
+testSubBoard3_1 = InProgress [[Just X, Just O, Nothing], [Nothing, Nothing, Nothing], [Just O, Just O, Nothing]]
+testSubBoard1_2 = InProgress [[Nothing, Just X, Just X], [Nothing, Just O, Nothing], [Nothing, Nothing, Nothing]]
+testSubBoard2_3 = InProgress [[Nothing, Nothing, Nothing], [Nothing, Nothing, Nothing], [Just O, Nothing, Just X]]
+
+
+testBoard :: GameState
+testBoard = (X, Just (2, 3), [[testSubBoard1_1, testSubBoard2_1, testSubBoard3_1],
+                              [testSubBoard1_2, Finished (Champ X), Finished (Champ O)],
+                              [Finished (Champ X), testSubBoard2_3, Finished (Champ O)]])
