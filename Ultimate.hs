e3bfa74ee64878770e8bf79a67b3b0dc5d5b5519
Jorge Larach <jorgelarach@jorges-macbook.trinity.local>
2023-11-03 12:31:58 -0500

    testing findlegalmoves branch

--- Ultimate.hs
+++ Ultimate.hs
@@ -4,7 +4,7 @@ data SubBoard = InProgress [[Maybe Player]] | Finished Winner deriving (Show, Eq
 type Coord = (Int, Int)
 type BigMove = (Coord, Coord) -- first coord is the location of the SUBBOARD second is for the spot on the subboard
 type GameState = (Player, Maybe Coord, [[SubBoard]])
--- GameState should be a type, not a variable type def
+-- findlegalmoves branch
 
 -- functions for
   -- winner of a game state (GameState -> Winner) Joseph
