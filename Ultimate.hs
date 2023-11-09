1a688698aef217a5d1115b4e74541e387bb126d8
Joseph C <mcangemi@trinity.edu>
2023-11-08 20:14:45 -0600

    Almost done

--- Ultimate.hs
+++ Ultimate.hs
@@ -21,7 +21,7 @@ testSubBoard3_2 = InProgress [[Just X, Just O, Just X], [Just O, Just X, Just X]
 
 mp3 = [[Just X, Just O, Just X], [Just O, Just X, Just X], [Just O, Just X, Just O]]
 mp4 = [[Just X, Just O, Just X], [Just O, Nothing, Just X], [Just O, Just X, Just O]]
---testSubBoard2_3 = Finished Champ X
+testSubBoard2_3 = Finished (Champ X)
 -- GameState should be a type, not a variable type def
 
 -- functions for
@@ -42,18 +42,40 @@ winnerB (p, mc, sbs) = undefined
 
 --missing case for full board tie game. Functionality not confirmed.XX
 
---checkLine :: Line -> SubBoard -> Maybe Winner
---checkLine (ci:ca:co) sb  = undefined
---if ci == Finished x && ci == ca == co then Just x else Nothing
+-- checkLine :: Line -> SubBoard -> Maybe Winner
+-- checkLine (ci:ca:co) sb  = undefined
+-- if ci == Finished x && ci == ca == co then Just x else Nothing
 --fix Logic?XXXXX
 
--- miniWinner :: SubBoard -> SubBoard
--- miniWinner sb = 
---   case sb of
---     Finished sb -> sb 
---     --checkWinner inProgress sb /= Nothing -> Just checkWinner inProgress sb
---     isFull inProgress sb -> isFull inProgress sb
+miniWinner :: SubBoard -> SubBoard
+miniWinner sb = if not(isInProgress sb) then sb 
+  else if (checkWinner sb) /= Nothing then Finished (fromJust (checkWinner sb))
+  else isFull sb
 
+
+checkWinner :: SubBoard -> Maybe Winner
+checkWinner sb = if checkDia sb /= Nothing then Just (Champ (fromJust (checkDia sb)))
+                         else if checkRow sb /= Nothing then Just (Champ (fromJust (checkRow sb)))
+                         else if checkCol sb /= Nothing then Just (Champ (fromJust (checkCol sb)))
+                         else Nothing
+--not done
+
+checkDia :: SubBoard -> Maybe Player
+checkDia ((InProgress(i:x:t)):(InProgress(a:xs:ts)):(InProgress(o:xxs:tts))) = if (xs /= Nothing && ((i == xs && (head tts) == xs ) || (o == xs && (head t) == xs))) then xs else Nothing
+
+checkRow :: SubBoard -> Maybe Player
+checkRow [] = Nothing
+checkRow (InProgress(x:xs)) = if (length[p | p <- x, p == X]==3 || length[p | p <- x, p == O]==3) then head x else checkRow xs 
+-- ==3 means exactly 3 to win 
+
+checkCol :: SubBoard -> Maybe Player
+checkCol [] = Nothing
+checkCol ((i:x):(a:xs):(o:xxs)) = 
+  if i /= Nothing && a == i && o == i then i else checkCol (x:xs:xxs)
+
+
+--assignCoordinates :: SubBoard -> [(Coord, Maybe Player)]
+--assignCoordinates (InProgress lst) = [((x, y), play) | (row, x) <- zip lst [1..], (play, y) <- zip row [1..]]
 
 --  | map
 --  | isFull sb = Tie
@@ -62,7 +84,7 @@ winnerB (p, mc, sbs) = undefined
 --miniWinner sb = let
   --winLine = [l |l <-lineLst, (checkLine l) /= Nothing]
   --if winLine  =
-isFull :: [[Maybe Player]] -> SubBoard
+isFull :: SubBoard -> SubBoard
 isFull sb = if length[s |s <- sb, length(catMaybes s)==3]==3 then Finished Tie else InProgress sb
 
 --[] = Finished Tie
