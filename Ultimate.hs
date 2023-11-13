6f88550253c267a237eda681c55dbde36a92a28f
Joseph C <mcangemi@trinity.edu>
2023-11-13 13:11:56 -0600

    Sprint 1 done

--- Ultimate.hs
+++ Ultimate.hs
@@ -1,4 +1,5 @@
 import Data.Maybe
+import Data.List
 
 data Player = X | O deriving (Show, Eq)
 data Winner = Champ Player | Tie deriving (Show, Eq)
@@ -6,22 +7,12 @@ data SubBoard = InProgress [[Maybe Player]] | Finished Winner deriving (Show, Eq
 type Coord = (Int, Int)
 type BigMove = (Coord, Coord) -- first coord is the location of the SUBBOARD second is for the spot on the subboard
 type GameState = (Player, Maybe Coord, [[SubBoard]])
-type Line = [Coord] --added line typeXX
+--type Line = [Coord] --added line typeXX
 
 --list of list of cordinates that form winning linesXX
-lineLst = [[(1,1),(2,2),(3,3)],[(3,1),(2,2),(1,3)],[(1,1),(1,2),(1,3)],
-           [(2,1),(2,2),(2,3)],[(3,1),(3,2),(3,3)],[(1,1),(2,1),(3,1)],[(1,2),(2,2),(3,2)],[(1,3),(2,3),(3,3)]]
+--lineLst = [[(1,1),(2,2),(3,3)],[(3,1),(2,2),(1,3)],[(1,1),(1,2),(1,3)],
+--           [(2,1),(2,2),(2,3)],[(3,1),(3,2),(3,3)],[(1,1),(2,1),(3,1)],[(1,2),(2,2),(3,2)],[(1,3),(2,3),(3,3)]]
 
-testLine = [(1,1),(1,2),(1,3)]
-testSubBoard1_1 = InProgress [[Just X, Nothing, Just O], [Just X, Just X, Just X], [Nothing, Nothing, Just O]]
-testSubBoard2_1 = InProgress [[Just O, Nothing, Nothing], [Just X, Just X, Nothing], [Nothing, Nothing, Nothing]]
-testSubBoard3_1 = InProgress [[Just X, Just O, Nothing], [Nothing, Nothing, Nothing], [Just O, Just O, Nothing]]
-testSubBoard1_2 = InProgress [[Nothing, Just X, Just X], [Nothing, Just O, Nothing], [Nothing, Nothing, Nothing]]
-testSubBoard3_2 = InProgress [[Just X, Just O, Just X], [Just O, Just X, Just X], [Just O, Just X, Just O]]
-
-mp3 = [[Just X, Just O, Just X], [Just O, Just X, Just X], [Just O, Just X, Just O]]
-mp4 = [[Just X, Just O, Just X], [Just O, Nothing, Just X], [Just O, Just X, Just O]]
-testSubBoard2_3 = Finished (Champ X)
 -- GameState should be a type, not a variable type def
 
 -- functions for
@@ -47,31 +38,56 @@ winnerB (p, mc, sbs) = undefined
 -- if ci == Finished x && ci == ca == co then Just x else Nothing
 --fix Logic?XXXXX
 
-miniWinner :: SubBoard -> SubBoard
-miniWinner sb = if not(isInProgress sb) then sb 
-  else if (checkWinner sb) /= Nothing then Finished (fromJust (checkWinner sb))
-  else isFull sb
+miniWinner :: SubBoard -> Maybe Winner
+miniWinner sb = let
+  cs = (checkWinner sb)
+  in if not (null cs) then cs
+  else if isFull sb then Just Tie else Nothing
 
 
 checkWinner :: SubBoard -> Maybe Winner
-checkWinner sb = if checkDia sb /= Nothing then Just (Champ (fromJust (checkDia sb)))
-                         else if checkRow sb /= Nothing then Just (Champ (fromJust (checkRow sb)))
-                         else if checkCol sb /= Nothing then Just (Champ (fromJust (checkCol sb)))
-                         else Nothing
---not done
+checkWinner sb = let
+  cd = checkDia sb
+  cr = checkRow sb
+  cc = checkCol sb
+            in if  cd /= Nothing then Just (Champ (fromJust cd))
+            else if cr /= Nothing then Just (Champ (fromJust cr))
+            else if cc /= Nothing then Just (Champ (fromJust cc))
+            else Nothing
 
 checkDia :: SubBoard -> Maybe Player
-checkDia ((InProgress(i:x:t)):(InProgress(a:xs:ts)):(InProgress(o:xxs:tts))) = if (xs /= Nothing && ((i == xs && (head tts) == xs ) || (o == xs && (head t) == xs))) then xs else Nothing
+checkDia (InProgress sb) = let
+  tl = head (head sb)
+  tr = last (head sb)
+  mid = head (tail (head (tail sb)))
+  bl = head (last sb)
+  br = last (last sb)
+  in if ((tl == mid && mid == br) || (tr == mid && mid == bl)) then mid else Nothing
+--(InProgress((i:x:t):(a:xs:ts):(o:xxs:tts))) = if (xs /= Nothing && ((i == xs && (head tts) == (head xs) ) || (o == xs && (head t)
+-- = xs))) then xs else Nothing
 
 checkRow :: SubBoard -> Maybe Player
-checkRow [] = Nothing
-checkRow (InProgress(x:xs)) = if (length[p | p <- x, p == X]==3 || length[p | p <- x, p == O]==3) then head x else checkRow xs 
+checkRow (InProgress b) = firstJust $ map aux b -- if any (aux X) board then Just X else if any (aux O) board then Just O else Nothing
+  where
+    aux [Just X, Just X, Just X] = Just X
+    aux [Just O,Just O,Just O] = Just O
+    aux _ = Nothing
+
+--checkRow [] = Nothing
+--checkRow (InProgress(x:xs)) = if (length[p | p <- x, p == X]==3 || length[p | p <- x, p == O]==3) then head x else checkRow xs 
+
+firstJust :: [Maybe a] -> Maybe a
+firstJust [] = Nothing
+firstJust (Nothing:vs) = firstJust vs
+firstJust (Just v:_)= Just v
+
 -- ==3 means exactly 3 to win 
 
 checkCol :: SubBoard -> Maybe Player
-checkCol [] = Nothing
-checkCol ((i:x):(a:xs):(o:xxs)) = 
-  if i /= Nothing && a == i && o == i then i else checkCol (x:xs:xxs)
+checkCol (InProgress b) = checkRow (InProgress(transpose b))
+-- checkCol [] = Nothing
+-- checkCol (InProgress((i:x):(a:xs):(o:xxs))) = 
+--   if i /= Nothing && a == i && o == i then i else checkCol (x:xs:xxs)
 
 
 --assignCoordinates :: SubBoard -> [(Coord, Maybe Player)]
@@ -84,8 +100,8 @@ checkCol ((i:x):(a:xs):(o:xxs)) =
 --miniWinner sb = let
   --winLine = [l |l <-lineLst, (checkLine l) /= Nothing]
   --if winLine  =
-isFull :: SubBoard -> SubBoard
-isFull sb = if length[s |s <- sb, length(catMaybes s)==3]==3 then Finished Tie else InProgress sb
+isFull :: SubBoard -> Bool
+isFull (InProgress p) = not $ any (==Nothing) (concat p) -- if length[s |s <- p, length(catMaybes s)==3]==3 then Finished Tie else InProgress p
 
 --[] = Finished Tie
 --isFull (s:sb) = if length (catMaybes s) == 3 then isFull sb else sb
